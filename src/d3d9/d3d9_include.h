35d1f297fc645ea4f13e687c0ad855d426dd4e5b
AGBOOLA ISRAEL OLUWAGBOGO <israelagboola53@gmail.com>
2026-09-15 12:30:00 +0000

    fix: Move D3DDEVINFO_RESOURCEMANAGER into dxvk namespace
    Upstream DXVK fix (62b99d7b). Newer MinGW-w64 headers define
    _D3DDEVINFO_RESOURCEMANAGER in global namespace. Moving our
    definition into the dxvk namespace avoids redefinition error.

--- src/d3d9/d3d9_include.h
+++ src/d3d9/d3d9_include.h
@@ -77,12 +77,15 @@
 #define D3DPRESENT_FORCEIMMEDIATE              0x00000100L
 #endif
 
-// MinGW headers are broken. Who'dve guessed?
+// Missing in some versions of mingw headers
 #ifndef _MSC_VER
-typedef struct _D3DDEVINFO_RESOURCEMANAGER
-{
-  char dummy;
-} D3DDEVINFO_RESOURCEMANAGER, * LPD3DDEVINFO_RESOURCEMANAGER;
+namespace dxvk {
+  typedef struct _D3DDEVINFO_RESOURCEMANAGER
+  {
+    char dummy;
+  } D3DDEVINFO_RESOURCEMANAGER, * LPD3DDEVINFO_RESOURCEMANAGER;
+}
+#endif
 
 #ifndef __WINE__
 extern "C" WINUSERAPI WINBOOL WINAPI SetProcessDPIAware(VOID);
